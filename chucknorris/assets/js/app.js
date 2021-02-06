35f1650b0dc6bdd6e95c5df5b9a90da4aa57dc2c
Raphael Borges <rah-borges@hotmail.com>
2021-02-05 22:12:51 -0300

    feat(aula5):desafio alpha

--- chucknorris/assets/js/app.js
+++ chucknorris/assets/js/app.js
@@ -5,16 +5,54 @@ const METHOD = 'GET'
 const CONTENT_TYPE = 'application/json'
 const MODE = 'cors'
 
+var loader = document.getElementById('loader')
+var root = document.getElementById('root')
+
+var formSearch = document.getElementById('search-form')
+
+function addJoke(joke){
+    loader = ""
+    root.innerHTML = `<div class="joke-box">
+    <img id="avatar" src="${joke.icon_url}" alt="">
+    <p id="joke">${joke.value}</p>
+    </div>`
+}
+
+formSearch.addEventListener('submit',function(e){
+    e.preventDefault()
+    loader.innerHTML = `<img src="assets/img/loader.gif" >`
+    let query = document.getElementById('theme-input').value
+    let jokes = getJokeByQuery(query)
+    jokes.forEach(joke => {
+        addJoke(joke)
+    })
+})
 
 async function getJoke(category){
+    loader.innerHTML = `<img src="assets/img/loader.gif" >`
     const joke = await getRandomJokeFromCategory(category)
+    addJoke(joke)
 
-    let imgNode = document.getElementById('avatar')
-    let jokeParagraph = document.getElementById("joke")
+}
+async function getJoke(){
+    loader.innerHTML = `<img src="assets/img/loader.gif" >`
+    const joke = await getRandomJoke()
+    addJoke(joke)
+}
 
-    imgNode.src = joke.icon_url
-    jokeParagraph.innerHTML = joke.value
-
+async function getJokeByQuery(query){
+    try{
+        const response = await fetch(`${BASE_URL}search?query=${query}`,{
+            methood: METHOD,
+            mode: MODE,
+            headers:{
+                'Content-Type': CONTENT_TYPE,
+            }
+        })
+        return await response.json();
+    }catch (error){
+        console.log(error);
+    }
 }
 
 async function getRandomJoke(){
@@ -36,7 +74,7 @@ async function getRandomJoke(){
 // busca de piada randomica apartir de uma categoria
 async function getRandomJokeFromCategory(category){
     try{
-        const response = await fetch(`${BASE_URL}random?catgory=${category}`,{
+        const response = await fetch(`${BASE_URL}random?category=${category}`,{
             methood: METHOD,
             mode: MODE,
             headers:{
@@ -49,3 +87,41 @@ async function getRandomJokeFromCategory(category){
     } 
 }
 
+async function getCategories(){
+    try{
+        const response = await fetch(`${BASE_URL}categories`,{
+            methood: METHOD,
+            mode: MODE,
+            headers:{
+                'Content-Type': CONTENT_TYPE,
+            }
+        })
+        return categories = await response.json()
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function builMenu(){
+    try{
+        let nav = document.getElementById('categories')
+        let categories = await getCategories()
+        
+        categories.forEach(category => {
+            let li = document.createElement('li')
+            // li.onclick = getJoke(category)
+            console.log(category);
+            li.value = category
+            li.innerHTML = category
+            li.addEventListener('click',getJoke(category))
+            nav.appendChild(li)
+        });
+        
+    }catch(error){
+        console.log(error)
+    }
+}
+
+
+builMenu()
+getJoke()
